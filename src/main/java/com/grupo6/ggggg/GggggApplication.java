caaf0b66647a1db94a67243154dc7e2a2f331426
Y0yoyopi <you@example.com>
2024-06-01 15:51:36 -0500

    f1

---
+++ src/main/java/com/grupo6/ggggg/GggggApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.ggggg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GggggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GggggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo6/ggggg/GggggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.ggggg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GggggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
