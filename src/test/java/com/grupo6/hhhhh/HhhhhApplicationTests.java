618360bbc2a25367b1e2f6c53bbd643d1baf5a55
Y0yoyopi <you@example.com>
2024-06-01 15:35:17 -0500

    Añadir archivos del proyecto local

---
+++ src/test/java/com/grupo6/hhhhh/HhhhhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.hhhhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhhhhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
